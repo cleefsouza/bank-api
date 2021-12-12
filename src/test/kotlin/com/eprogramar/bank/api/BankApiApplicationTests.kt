16d88700273839679ebc95e04cdb0f3b186ef8a4
Aryosvalldo Cleef <a.souza.ext@conductor.com.br>
2021-12-12 15:27:13 -0300

    Testes automatizados

---
+++ src/test/kotlin/com/eprogramar/bank/api/BankApiApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.eprogramar.bank.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BankApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+}
